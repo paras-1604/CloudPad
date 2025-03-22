5f0e1bc6b1f60c4892605643d3879a345975d0ff
Srushti Raskar <sraskar228@gmail.com>
2025-03-22 07:32:07 -0700

    Update MainWindow.java

--- src/main/java/com/cloudpad/MainWindow.java
+++ src/main/java/com/cloudpad/MainWindow.java
@@ -1,66 +1,94 @@
-
-package com.cloudpad;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
+public class MainWindow extends JFrame implements ActionListener
+{
+    JMenuItem saveFileMenuItem;
+    JTextArea textarea;
 
-
-public class MainWindow extends JFrame{
-
-    MainWindow(){
-
+    MainWindow() 
+    {
         JMenuBar menuBar = new JMenuBar();
-        JTextArea textarea = new JTextArea();
+        textarea = new JTextArea();
         JFrame frame = new JFrame();
-        frame.setTitle("demo bar");
-        frame.setSize(350,500);
-        frame.setVisible(true);
-    
+        frame.setTitle("Demo Bar");
+        frame.setSize(400, 500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         JMenu filemenu = new JMenu("File");
-        JMenu editmenu = new JMenu("edit");
-        JMenu helpmenu = new JMenu("help");
-    
-        JMenuItem newFileItem =  new JMenuItem("New File");
-        JMenuItem newFolderItem =  new JMenuItem("New Folder");
-        JMenuItem openFileMenuItem = new JMenuItem("open file");
-        JMenuItem openFolderMenuItem = new JMenuItem("open folder");
-        JMenuItem saveFileMenuItem = new JMenuItem("Save");
+        JMenu editmenu = new JMenu("Edit");
+        JMenu helpmenu = new JMenu("Help");
+
+        int const1 = ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS;
+        int const2 = ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS;
+        JScrollPane sp = new JScrollPane(textarea, const1, const2);
+
+        JMenuItem newFileItem = new JMenuItem("New File");
+        JMenuItem newFolderItem = new JMenuItem("New Folder");
+        JMenuItem openFileMenuItem = new JMenuItem("Open File");
+        JMenuItem openFolderMenuItem = new JMenuItem("Open Folder");
+        saveFileMenuItem = new JMenuItem("Save");
         JMenuItem saveAllFilesMenuItem = new JMenuItem("Save All");
         JMenuItem exitMenuItem = new JMenuItem("Exit");
-    
-        JMenuItem copyItem = new JMenuItem("copy");
+
+        JMenuItem copyItem = new JMenuItem("Copy");
         JMenuItem pasteItem = new JMenuItem("Paste");
-        JMenuItem cutItem = new JMenuItem("cut");
-    
+        JMenuItem cutItem = new JMenuItem("Cut");
+
         JMenuItem helpItem = new JMenuItem("Help");
         JMenuItem aboutItem = new JMenuItem("About");
-    
+
         filemenu.add(newFileItem);
         filemenu.add(newFolderItem);
         filemenu.add(openFileMenuItem);
-        
         filemenu.add(openFolderMenuItem);
         filemenu.add(saveFileMenuItem);
         filemenu.add(saveAllFilesMenuItem);
         filemenu.add(exitMenuItem);
-        
+
         editmenu.add(copyItem);
         editmenu.add(pasteItem);
         editmenu.add(cutItem);
-    
+
         helpmenu.add(helpItem);
         helpmenu.add(aboutItem);
-    
+
         menuBar.add(filemenu);
-       // menuBar.addSeparator();
         menuBar.add(editmenu);
         menuBar.add(helpmenu);
-    
-        frame.add(menuBar,BorderLayout.NORTH);
-        frame.add(textarea, BorderLayout.CENTER);
+
+        frame.add(menuBar, BorderLayout.NORTH);
+        frame.add(sp);
         frame.setVisible(true);
+
+        saveFileMenuItem.addActionListener(this);
+        exitMenuItem.addActionListener(e -> System.exit(0));
     }
-   public static void main(String[] args) {
-    new MainWindow();
-   }
-}
+
+    public void actionPerformed(ActionEvent ae) 
+    {
+        if (ae.getSource() == saveFileMenuItem) 
+        {
+            JFileChooser fileChooser = new JFileChooser();
+            int option = fileChooser.showSaveDialog(this);
+            if (option == JFileChooser.APPROVE_OPTION) 
+            {
+                File file = fileChooser.getSelectedFile();
+                try (FileWriter fw = new FileWriter(file)) 
+                {
+                    fw.write(textarea.getText());
+                } 
+                catch (IOException e) 
+                {
+                    JOptionPane.showMessageDialog(this, "Error saving file", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+} 
